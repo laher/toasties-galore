218bf93fdfc3cd1338c78c30082c5abf5a6675c5
Am Laher <am.laher@vendhq.com>
2019-10-20 09:25:21 +1300

    implement Pick V2 endpoint

--- chillybin/handler.go
+++ chillybin/handler.go
@@ -46,9 +46,8 @@ func (h *handler) checkStock(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
-func (h *handler) pick(w http.ResponseWriter, r *http.Request) {
+func (h *handler) pickV1(w http.ResponseWriter, r *http.Request) {
 	var (
-		query    = "UPDATE ingredients SET quantity= quantity - $2 WHERE name=$1 and quantity - $2 >= 0"
 		values   = r.URL.Query()
 		n        = values.Get("name")
 		q        = values.Get("quantity")
@@ -61,31 +60,102 @@ func (h *handler) pick(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("input error - quantity must be integer"))
 		return
 	}
-	res, err := h.db.Exec(query, n, quantity)
+	if err := h.doPick(w, r, h.db, n, quantity); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error fetching ingredient"))
+	}
+	w.Write([]byte("ok"))
+}
+
+type PickRequest struct {
+	Ingredient string
+	Quantity   int
+	Customer   string
+	// TODO some other fields here
+}
+
+func (h *handler) pickV2(w http.ResponseWriter, r *http.Request) {
+	var (
+		m = PickRequest{}
+	)
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tx, err := h.db.Begin()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error fetching ingredient"))
+		return
+	}
+	var (
+		query = "INSERT INTO orders (customer, ingredient, quantity, date) VALUES ($1, $2, $3, NOW()) RETURNING id"
+	)
+	res, err := tx.Exec(query, m.Customer, m.Ingredient, m.Quantity)
 	if err != nil {
 		switch e := err.(type) {
 		case *pq.Error:
 			log.Printf("pq error: %s", e.Code.Name())
 		}
-		log.Printf("Error fetching ingredient %s with quantity '%s': %v", n, q, err)
+		log.Printf("Error creating order: '%+v', %v", m, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("error fetching ingredient"))
 		return
+	}
+	if err := h.doPick(w, r, tx, m.Ingredient, m.Quantity); err != nil {
+		tx.Rollback()
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error fetching ingredient"))
+		return
+	}
+	if err := tx.Commit(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error fetching ingredient"))
+		return
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error fetching ingredient"))
+		return
+	}
+	b, err := json.Marshal(map[string]int64{"id": id})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error fetching ingredient"))
+		return
+	}
+	w.Write(b)
+
+}
+
+type execer interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+func (h *handler) doPick(w http.ResponseWriter, r *http.Request, db execer, n string, quantity int) error {
+	var (
+		query = "UPDATE ingredients SET quantity= quantity - $2 WHERE name=$1 and quantity - $2 >= 0"
+	)
+	res, err := db.Exec(query, n, quantity)
+	if err != nil {
+		switch e := err.(type) {
+		case *pq.Error:
+			log.Printf("pq error: %s", e.Code.Name())
+		}
+		log.Printf("Error fetching ingredient %s with quantity '%d': %v", n, quantity, err)
+		return err
 	}
 	affected, err := res.RowsAffected()
 	if err != nil || affected > 1 {
-		log.Printf("Error fetching ingredient %s with quantity '%s': %v", n, q, err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error fetching ingredient"))
-		return
+		log.Printf("Error fetching ingredient %s with quantity '%d': %v", n, quantity, err)
+		return err
 	}
 	if affected < 1 {
-		log.Printf("Error fetching ingredient %s with quantity '%s': 0 rows affected", n, q)
-		w.WriteHeader(http.StatusGone)
-		w.Write([]byte("could not find requested ingredient"))
-		return
+		log.Printf("Error fetching ingredient %s with quantity '%d': 0 rows affected", n, quantity)
+		return err
 	}
-	w.Write([]byte("ok"))
+	return nil
 }
 
 // restock all ingredients
@@ -124,7 +194,8 @@ func routes(h *handler, version string) http.Handler {
 	router.HandleFunc("/", h.checkStock)
 
 	// pick ingredient by name and quantity
-	router.HandleFunc("/pick", h.pick)
+	router.HandleFunc("/pick", h.pickV1)    // DEPRECATED route
+	router.HandleFunc("/v2/pick", h.pickV2) // Updated route
 
 	// restock all ingredients
 
